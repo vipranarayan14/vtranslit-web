1c7b7576796e68e646d5521d4451206139374542
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-05-07 19:22:37 +0530

    make webpack to output 'scripts.js' and 'styles.css' with only first 4 chars of hash in name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const entry = './prod/app.js';
-const filename = 'scripts.[chunkhash].js';
+const filename = 'scripts.[chunkhash:4].js';
 
 const config = {
   entry,
@@ -51,7 +51,7 @@ const config = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('styles.[contenthash].css'),
+    new ExtractTextPlugin('styles.[contenthash:4].css'),
     new UglifyJsPlugin(),
     new CopyWebpackPlugin([{
       from: 'prod/assets/icons/**/*',
