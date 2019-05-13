3b1a458c7fcff43f35bd7f097b80b3033d4ee7a2
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2019-05-13 14:17:22 +0530

    add styles for `ToolBar`. make `ToolBar` to render children if given to allow extending the component.

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './ToolBar.module.css';
+
 import { availableSchemes } from './../libs/vtranslit';
 
-export const ToolBar = ({ type, handleSchemeSelect, defaultSelectValue }) => (
-  <select onChange={handleSchemeSelect} defaultValue={defaultSelectValue}>
-    {availableSchemes.map((scheme, index) => (
-      <option value={scheme.code} key={index}>
-        {scheme.name}
-      </option>
-    ))}
-  </select>
+export const ToolBar = ({ handleSchemeChange, defaultScheme, children }) => (
+  <div className={styles.ToolBar}>
+    <select
+      onChange={handleSchemeChange}
+      defaultValue={defaultScheme}
+      className={styles.Tool}
+    >
+      {availableSchemes.map((scheme, index) => (
+        <option value={scheme.code} key={index}>
+          {scheme.name}
+        </option>
+      ))}
+    </select>
+    {children}
+  </div>
 );
 
 ToolBar.propTypes = {
-  handleSchemeSelect: PropTypes.func.isRequired
+  handleSchemeChange: PropTypes.func.isRequired,
+  defaultScheme: PropTypes.string.isRequired
 };
